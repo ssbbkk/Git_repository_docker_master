31abb0d3103db2cf4df2e75c8bc68a868ce720a7
ssbbkk <sebastian.boloz88@gmail.com>
2018-04-15 23:59:10 +0200

    File some_scp_script.sh modified

--- some_scp_script.sh
+++ some_scp_script.sh
@@ -2,8 +2,8 @@
 
 # This is a test script v0.2
 
-cp -p /root/my_git_dir/some_script.sh /root/my_git_dir/$(date)_some_script.sh
+cp -p /root/my_git_dir/some_script.sh /root/my_git_dir/some_script.sh
 
-scp -p /root/my_git_dir/2*some_script.sh test_sc@192.168.100.15:/home/test_sc/incoming_scripts/
+scp -p /root/my_git_dir/some_script.sh test_sc@192.168.100.15:/home/test_sc/incoming_scripts/
 
 echo "Script has been sent to test_sc@192.168.100.15."
